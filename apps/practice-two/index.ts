232276e623f7a3659f6b4c9a16cde736b7a66fb5
aleksandarz <zaleksandar888@gmail.com>
2026-02-05 17:28:37 +0100

    Added more code to the homework and updated it

--- apps/practice-two/index.ts
+++ apps/practice-two/index.ts
@@ -4,20 +4,47 @@ const http = require("http");
 const server = http.createServer((req: any, res: any) => {
   res.setHeader("Content-Type", "text/plain");
 
-  if (req.url === "/") {
-    res.statusCode = 200;
-    res.end("Welcome to the main page");
-  } else if (req.url === "/contact") {
-    res.statusCode = 302;
-    res.setHeader("Location", "/")
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.end("Page Not Found");
+  const data: string[] = ["Aleksandar", "Mina", "Lola", "Vlada"];
+
+  console.log(req.method);
+
+  if (req.method === "POST") {
+    res.statusCode = 405;
+    res.end("Method Not Allowed");
+    return;
+  }
+
+  switch (req.url) {
+    case "/":
+      res.statusCode = 200;
+      res.end("Welcome to the main page");
+      break;
+    case "/about":
+      res.statusCode = 200;
+      res.end("Welcome to the about page");
+      break;
+    case "/contact":
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      res.end();
+      break;
+    case "/api/users":
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(data));
+      break;
+    case "/redirect-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      res.statusCode = 404;
+      res.end("Not Found");
+      break;
   }
 
 });
 
 server.listen(3000);
-
-console.log("Server is running at localhost - http://localhost:3000/");
+console.log("Server is running at localhost - http://localhost:3000");
