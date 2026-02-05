75cd768002e5eb1aa5580df3cbef55cb2d7bcaf0
aleksandarz <zaleksandar888@gmail.com>
2026-02-05 14:48:09 +0100

    Added practice two homework

--- apps/practice-two/index.ts
+++ apps/practice-two/index.ts
@@ -7,6 +7,10 @@ const server = http.createServer((req: any, res: any) => {
   if (req.url === "/") {
     res.statusCode = 200;
     res.end("Welcome to the main page");
+  } else if (req.url === "/contact") {
+    res.statusCode = 302;
+    res.setHeader("Location", "/")
+    res.end();
   } else {
     res.statusCode = 404;
     res.end("Page Not Found");
